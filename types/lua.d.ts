e14e8cf9a5c445b42e7d3d7629d8d7777b0b8895
Glen Reesor <glen@wiseoldtree.ca>
2022-05-14 16:09:06 -0600

    Fix lua type for print() -- argument is optional

--- types/lua.d.ts
+++ types/lua.d.ts
@@ -1,4 +1,4 @@
-declare function print(this: void, text: string | number): void
+declare function print(this: void, text?: string | number): void
 
 declare namespace os {
   function getenv(this: void, envVariable: string): string;
