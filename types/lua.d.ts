288f86bfb3d3a30571e6cabcc7d19a0d13931a76
Glen Reesor <glen@wiseoldtree.ca>
2024-01-13 21:53:08 -0700

    Add simple text widget and hard-coded path to local shell script

--- types/lua.d.ts
+++ types/lua.d.ts
@@ -1,3 +1,20 @@
+// Copyright 2024 Glen Reesor
+//
+// This file is part of HammerBar.
+//
+// HammerBar is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// HammerBar is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// HammerBar. If not, see <https://www.gnu.org/licenses/>.
+
 declare function print(this: void, text?: string | number): void
 
 declare namespace os {
@@ -15,4 +32,8 @@ declare namespace os {
 
   function date(this: void, format: string, time?: number): string | DateTable;
   function getenv(this: void, envVariable: string): string;
+}
+
+declare namespace io {
+  function popen(this: void, something: string, somethingElse: string): any;
 }
