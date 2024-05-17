10a7a3636b58dac761306f66fee7154deb54efb8
Glen Reesor <glen@wiseoldtree.ca>
2024-05-17 16:27:15 -0600

    LineGraph: Add expanded view on click
    commit 0c1c57896e654211cc84d12ddf6b63204d1298b7
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Fri May 17 16:26:35 2024 -0600
    
        Hide expandedViewCanvas when taskbar is minimized
    
    commit e8407b9dc4006df9b62923dad94a5e7ba2c153b6
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Sat May 4 14:51:12 2024 -0600
    
        Round y-coordinate of horizontal line so we don't get some blurry and some sharp
    
    commit 09a72c0ee12fb3f49c34e28c1e558d1deb6b9330
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Sat May 4 14:48:24 2024 -0600
    
        Refactor to pull out getHorizontalScaleLineWithLabel()
    
        And show lines at (0, 25, 0.5, 0.75, 1) * Max
    
    commit a563a360662c89835dd94dd6896a72f211a03428
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Sat May 4 14:22:32 2024 -0600
    
        Fix: Ensure graph doesn't overlap blue indicator bar
    
    commit 51fa7e280b22965c834bbc1abae9446695d78ae6
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Sat May 4 14:15:42 2024 -0600
    
        Refactor to pull out getGraphLineSegments()
    
    commit 3509b84da997c08ac76c83baecedc0a6ecda827d
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Sat May 4 13:22:25 2024 -0600
    
        Refactor to pull out getGraphScaleFactors()
    
    commit 943556ebe5e09330d7669a20224e5ba470697da6
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Fri Apr 26 23:22:20 2024 -0600
    
        Add y-axis labels, indicator bar and current value
    
    commit 8130577cb8e790ec1e6833ea055e6ca12a53da16
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Fri Apr 26 22:51:55 2024 -0600
    
        Initial version of expandedView
    
    commit ee3075e11fcab80cb282cdf7001b665efd2a5df5
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Fri Apr 26 20:42:27 2024 -0600
    
        Move renderHoverValue() to better place in file
    
    commit 7f6ababfc50e9bc923bc3ce2d937ed2b44e6bd30
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Fri Apr 26 20:41:07 2024 -0600
    
        Add click effect that will soon get wired up to showing expandedView
    
    commit 6b6e61732db74309c4b533b0ac4244b9fb766375
    Author: Glen Reesor <glen@wiseoldtree.ca>
    Date:   Fri Apr 26 20:07:26 2024 -0600
    
        Add a hover effect

--- src/widgets/lineGraph.ts
+++ src/widgets/lineGraph.ts
@@ -35,6 +35,9 @@ export function getLineGraphBuilder(
     panelHoverColor,
   }: WidgetBuilderParams) {
     function cleanupPriorToDelete() {
+      state.expandedViewCanvas?.hide();
+      state.expandedViewCanvas = undefined;
+
       state.graphCanvas?.hide();
       state.graphCanvas = undefined;
 
@@ -44,6 +47,18 @@ export function getLineGraphBuilder(
       state.timer?.stop();
     }
 
+    function hide() {
+      state.graphCanvas?.hide();
+      state.hoverCanvas?.hide();
+      state.expandedViewCanvas?.hide();
+    }
+
+    function show() {
+      state.graphCanvas?.show();
+      state.hoverCanvas?.show();
+      state.expandedViewCanvas?.show();
+    }
+
     const mouseCallback: hs.CanvasMouseCallbackType = function (
       this: void,
       _canvas: hs.CanvasType,
@@ -51,17 +66,368 @@ export function getLineGraphBuilder(
     ) {
       if (msg === 'mouseEnter') {
         state.mouseIsInside = true;
-        renderHoveredValue();
+        updateShowHoverValue();
+        render();
       } else if (msg === 'mouseExit') {
         state.mouseIsInside = false;
-        if (state.hoverCanvas !== undefined) {
-          state.hoverCanvas.hide();
-          state.hoverCanvas = undefined;
+        updateShowHoverValue();
+        render();
+      } else if (msg === 'mouseDown') {
+        state.mouseButtonIsDown = true;
+        render();
+      } else if (msg === 'mouseUp') {
+        state.mouseButtonIsDown = false;
+        state.renderExpandedView = !state.renderExpandedView;
+
+        if (
+          !state.renderExpandedView &&
+          state.expandedViewCanvas !== undefined
+        ) {
+          state.expandedViewCanvas.hide();
+          state.expandedViewCanvas = undefined;
         }
+
+        if (state.renderExpandedView) {
+          renderExpandedView();
+        }
+
+        updateShowHoverValue();
+        render();
       }
     };
 
-    function renderHoveredValue() {
+    function updateShowHoverValue() {
+      state.renderHoverValue = state.mouseIsInside && !state.renderExpandedView;
+      if (state.renderHoverValue) {
+        renderHoverValue();
+      }
+      if (!state.renderHoverValue && state.hoverCanvas !== undefined) {
+        state.hoverCanvas.hide();
+        state.hoverCanvas = undefined;
+      }
+    }
+
+    function getGraphLineSegments(args: {
+      bgColor: hs.ColorType;
+      graphDimensions: { w: number; h: number };
+      graphTopLeft: { x: number; y: number };
+      scale: { x: number; y: number };
+      strokeColor: hs.ColorType;
+    }): hs.CanvasElementType[] {
+      const { bgColor, graphDimensions, graphTopLeft, scale, strokeColor } =
+        args;
+
+      return [
+        {
+          type: 'segments',
+          coordinates: state.values.map((value, i) => ({
+            x: graphTopLeft.x + i * scale.x,
+            y: graphTopLeft.y + graphDimensions.h - value * scale.y,
+          })),
+          fillColor: bgColor,
+          strokeColor,
+        },
+      ];
+    }
+
+    function getGraphScaleFactors(args: {
+      graphDimensions: { w: number; h: number };
+      maxYValue: number;
+      numValues: number;
+      shrinkIfMouseButtonDown: boolean;
+    }): { x: number; y: number } {
+      const { graphDimensions, maxYValue, numValues, shrinkIfMouseButtonDown } =
+        args;
+
+      const baseXScale = graphDimensions.w / numValues;
+      const xScale =
+        shrinkIfMouseButtonDown && state.mouseButtonIsDown
+          ? baseXScale * 0.8
+          : baseXScale;
+
+      const baseYScale = graphDimensions.h / maxYValue;
+      const yScale =
+        shrinkIfMouseButtonDown && state.mouseButtonIsDown
+          ? baseYScale * 0.8
+          : baseYScale;
+
+      return { x: xScale, y: yScale };
+    }
+
+    function getHorizontalScaleLineWithLabel(args: {
+      graphDimensions: { w: number; h: number };
+      graphTopLeft: { x: number; y: number };
+      scale: { x: number; y: number };
+      value: number;
+    }): hs.CanvasElementType[] {
+      const { graphDimensions, graphTopLeft, scale, value } = args;
+
+      const fontSize = 12;
+      const y = Math.round(
+        graphTopLeft.y + graphDimensions.h - value * scale.y,
+      );
+      const label = `${Math.round(value)}`;
+
+      return [
+        {
+          type: 'segments',
+          coordinates: [
+            {
+              x: graphTopLeft.x - 5,
+              y,
+            },
+            {
+              x: graphTopLeft.x + graphDimensions.w,
+              y,
+            },
+          ],
+          strokeColor: { red: 0.8, green: 0.8, blue: 0.8 },
+          strokeWidth: 1,
+        },
+        {
+          type: 'text',
+          text: label,
+          textColor: { red: 0.8, green: 0.8, blue: 0.8 },
+          textSize: fontSize * 0.8,
+          frame: {
+            x: 4,
+            y: y - fontSize / 2,
+            w: graphDimensions.w,
+            h: fontSize * 1.2,
+          },
+        },
+      ];
+    }
+
+    function render() {
+      const baseFontSize = 12;
+      const fontSize = state.mouseButtonIsDown
+        ? baseFontSize * 0.8
+        : baseFontSize;
+
+      const titleY = height / 2 - fontSize - fontSize / 2;
+      const bgColor = state.mouseIsInside
+        ? { red: 120 / 255, green: 140 / 255, blue: 140 / 255 }
+        : panelHoverColor;
+
+      const max =
+        maxGraphValue ??
+        state.values.reduce((acc, v) => (v > acc ? v : acc), 0);
+
+      const graphTopLeft = {
+        x: 0,
+        y: titleY + fontSize * 1.3,
+      };
+
+      const graphDimensions = {
+        w: width,
+        h: height - graphTopLeft.y,
+      };
+
+      const scale = getGraphScaleFactors({
+        graphDimensions,
+        maxYValue: max,
+        numValues: maxValues,
+        shrinkIfMouseButtonDown: true,
+      });
+
+      const maxString = `${Math.round(max)}`;
+
+      const mainCanvasElements: hs.CanvasElementType[] = [
+        {
+          type: 'rectangle',
+          fillColor: bgColor,
+          strokeColor: panelColor,
+          frame: {
+            x: 0,
+            y: 0,
+            w: width,
+            h: height,
+          },
+          trackMouseEnterExit: true,
+          trackMouseDown: true,
+          trackMouseUp: true,
+        },
+        {
+          type: 'text',
+          text: title,
+          textColor: BLACK,
+          textSize: fontSize,
+          frame: {
+            x: 2,
+            y: titleY,
+            w: width,
+            h: fontSize * 1.2,
+          },
+        },
+        {
+          // Max line
+          type: 'segments',
+          coordinates: [graphTopLeft, { x: width, y: graphTopLeft.y }],
+          strokeColor: { red: 1, green: 1, blue: 1 },
+          strokeWidth: 1,
+        },
+        {
+          // Max label
+          type: 'text',
+          text: maxString,
+          textAlignment: 'right',
+          textColor: WHITE,
+          textSize: fontSize * 0.8,
+          frame: {
+            x: 0,
+            y: graphTopLeft.y - fontSize,
+            w: width - 2,
+            h: fontSize * 1.2,
+          },
+        },
+      ];
+
+      const graphLineSegments = getGraphLineSegments({
+        bgColor: panelHoverColor,
+        graphDimensions,
+        graphTopLeft,
+        scale,
+        strokeColor: { red: 0, green: 1, blue: 1 },
+      });
+
+      state.graphCanvas?.replaceElements([
+        ...mainCanvasElements,
+        ...graphLineSegments,
+      ]);
+    }
+
+    function renderExpandedView() {
+      const expandedViewHeight = 150;
+      const expandedViewWidth = 150;
+
+      if (state.expandedViewCanvas === undefined) {
+        state.expandedViewCanvas = hs.canvas.new({
+          x: x - expandedViewWidth + width,
+          y: y - expandedViewHeight,
+          w: expandedViewWidth,
+          h: expandedViewHeight,
+        });
+        state.expandedViewCanvas.show();
+      }
+
+      const fontSize = 12;
+      const titleY = fontSize;
+
+      const max =
+        maxGraphValue ??
+        state.values.reduce((acc, v) => (v > acc ? v : acc), 0);
+
+      const currentValue = state.values[state.values.length - 1];
+
+      const graphTopLeft = {
+        x: fontSize * `${Math.round(max)}`.length,
+        y: titleY + fontSize * 2,
+      };
+
+      const indicatorBarHeight = 2;
+      const indicatorBarPadding = 2;
+
+      const graphDimensions = {
+        w: expandedViewWidth - graphTopLeft.x,
+        h:
+          expandedViewHeight -
+          graphTopLeft.y -
+          indicatorBarHeight -
+          indicatorBarPadding,
+      };
+
+      const scale = getGraphScaleFactors({
+        graphDimensions,
+        maxYValue: max,
+        numValues: maxValues,
+        shrinkIfMouseButtonDown: false,
+      });
+
+      const currentValueString = `${Math.round(currentValue)}`;
+
+      const mainCanvasElements: hs.CanvasElementType[] = [
+        {
+          type: 'rectangle',
+          fillColor: { red: 0.5, green: 0.5, blue: 0.5 },
+          strokeColor: { red: 0.4, green: 0.4, blue: 0.4 },
+          frame: {
+            x: 0,
+            y: 0,
+            w: expandedViewWidth,
+            h: expandedViewHeight,
+          },
+          roundedRectRadii: { xRadius: 5.0, yRadius: 5.0 },
+        },
+        {
+          // Indicator bar over regular canvas
+          type: 'rectangle',
+          fillColor: { red: 0, green: 0, blue: 1 },
+          strokeColor: { red: 0, green: 0, blue: 1 },
+          frame: {
+            x: expandedViewWidth - width,
+            y: expandedViewHeight - indicatorBarHeight,
+            w: width,
+            h: indicatorBarHeight,
+          },
+          roundedRectRadii: { xRadius: 5.0, yRadius: 5.0 },
+        },
+        {
+          // Title
+          type: 'text',
+          text: title,
+          textColor: BLACK,
+          textSize: fontSize,
+          frame: {
+            x: 4,
+            y: titleY,
+            w: expandedViewWidth,
+            h: fontSize * 1.2,
+          },
+        },
+        {
+          // Current value
+          type: 'text',
+          text: currentValueString,
+          textAlignment: 'right',
+          textColor: WHITE,
+          textSize: fontSize,
+          frame: {
+            x: 0,
+            y: titleY,
+            w: expandedViewWidth / 2,
+            h: fontSize * 1.2,
+          },
+        },
+      ];
+
+      const graphLineSegments = getGraphLineSegments({
+        bgColor: { red: 0.5, green: 0.5, blue: 0.5 },
+        graphDimensions,
+        graphTopLeft,
+        scale,
+        strokeColor: { red: 0, green: 1, blue: 1 },
+      });
+
+      const horizontalScaleLinesWithLabels = [0, 0.25, 0.5, 0.75, 1].flatMap(
+        (fractionOfMax) => {
+          return getHorizontalScaleLineWithLabel({
+            graphDimensions,
+            graphTopLeft,
+            scale,
+            value: fractionOfMax * max,
+          });
+        },
+      );
+
+      state.expandedViewCanvas.replaceElements([
+        ...mainCanvasElements,
+        ...graphLineSegments,
+        ...horizontalScaleLinesWithLabels,
+      ]);
+    }
+
+    function renderHoverValue() {
       const fontSize = 10;
       const value = state.values[state.values.length - 1];
       const canvasX = x;
@@ -106,104 +472,40 @@ export function getLineGraphBuilder(
       state.hoverCanvas.show();
     }
 
-    function render() {
-      const fontSize = 12;
-      const titleY = height / 2 - fontSize - fontSize / 2;
-      const graphY = titleY + fontSize * 1.3;
-      const heightForGraph = height - graphY;
-
-      const max =
-        maxGraphValue ??
-        state.values.reduce((acc, v) => (v > acc ? v : acc), 0);
-
-      const xScale = width / maxValues;
-      const yScale = heightForGraph / max;
-
-      const maxString = `${Math.ceil(max)}`;
-
-      state.graphCanvas?.replaceElements([
-        {
-          type: 'rectangle',
-          fillColor: panelHoverColor,
-          strokeColor: panelColor,
-          frame: {
-            x: 0,
-            y: 0,
-            w: width,
-            h: height,
-          },
-          trackMouseEnterExit: true,
-        },
-        {
-          type: 'text',
-          text: title,
-          textColor: BLACK,
-          textSize: fontSize,
-          frame: {
-            x: 2,
-            y: titleY,
-            w: width,
-            h: fontSize * 1.2,
-          },
-        },
-        {
-          type: 'segments',
-          coordinates: state.values.map((value, i) => ({
-            x: i * xScale,
-            y: graphY + heightForGraph - value * yScale,
-          })),
-          fillColor: panelHoverColor,
-          strokeColor: { red: 0, green: 1, blue: 1 },
-        },
-        {
-          // Max line
-          type: 'segments',
-          coordinates: [
-            { x: 0, y: graphY },
-            { x: width, y: graphY },
-          ],
-          strokeColor: { red: 1, green: 1, blue: 1 },
-          strokeWidth: 1,
-        },
-        {
-          // Max label
-          type: 'text',
-          text: maxString,
-          textAlignment: 'right',
-          textColor: WHITE,
-          textSize: fontSize * 0.8,
-          frame: {
-            x: 0,
-            y: graphY - fontSize,
-            w: width - 2,
-            h: fontSize * 1.2,
-          },
-        },
-      ]);
-    }
-
     function runCmdAndRender() {
       state.values.push(cmd());
       state.values = state.values.slice(-1 * maxValues);
       render();
 
-      if (state.mouseIsInside) {
-        renderHoveredValue();
+      if (state.renderHoverValue) {
+        renderHoverValue();
+      }
+
+      if (state.renderExpandedView) {
+        renderExpandedView();
       }
 
       state.timer = hs.timer.doAfter(interval, runCmdAndRender);
     }
 
     const state: {
+      expandedViewCanvas: hs.CanvasType | undefined;
       graphCanvas: hs.CanvasType | undefined;
       hoverCanvas: hs.CanvasType | undefined;
+      mouseButtonIsDown: boolean;
       mouseIsInside: boolean;
+      renderExpandedView: boolean;
+      renderHoverValue: boolean;
       timer?: hs.TimerType;
       values: number[];
     } = {
+      expandedViewCanvas: undefined,
       graphCanvas: undefined,
       hoverCanvas: undefined,
+      mouseButtonIsDown: false,
       mouseIsInside: false,
+      renderExpandedView: false,
+      renderHoverValue: false,
       values: [],
     };
 
@@ -217,8 +519,8 @@ export function getLineGraphBuilder(
     return {
       bringToFront: () => state.graphCanvas?.show(),
       cleanupPriorToDelete,
-      hide: () => state.graphCanvas?.hide(),
-      show: () => state.graphCanvas?.show(),
+      hide: hide,
+      show: show,
     };
   }
 
